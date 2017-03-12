f751a5fef4a07dd755be6a896b082b2dfd02387f
Аксеник Дмитрий < >
2017-03-12 15:56:27 +0300

    TP added

--- C++ Multithreading/Lab 2/ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ C++ Multithreading/Lab 2/ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <fstream>
 #include <Windows.h>
+#include <string>
 using namespace std;
 
-bool stop_interface;
+bool stop_interface, terminateCounter;
 double progress;
 
 int** calculate(int** a, int** b, int n) {
@@ -16,6 +18,10 @@ int** calculate(int** a, int** b, int n) {
   for (int i = 0; i < n; i++)
     for (int j = 0; j < n; j++)
       for (int k = 0; k < n; k++) {
+        if (terminateCounter) {
+          stop_interface = true;
+          return result;
+        }
         result[i][j] += a[i][k] * a[k][j];
         progress += 1 / pow(n, 3);
       }
@@ -23,22 +29,29 @@ int** calculate(int** a, int** b, int n) {
 }
 
 DWORD WINAPI counterThread(LPVOID param) {
-  int**a = new int*[100];
-  int**b = new int*[100];
-  for (int i = 0; i < 100; i++) {
-    a[i] = new int[100];
-    b[i] = new int[100];
+  const int SIZE = 5000;
+  int**a = new int*[SIZE];
+  int**b = new int*[SIZE];
+  for (int i = 0; i < SIZE; i++) {
+    a[i] = new int[SIZE];
+    b[i] = new int[SIZE];
   }
 
-  for(int i=0;i<100;i++)
-    for (int j = 0; j < 100; j++)
+  for(int i=0;i<SIZE;i++)
+    for (int j = 0; j < SIZE; j++)
       a[i][j] = b[i][j] = i == j ? 1 : 0;
 
-  int**ans = calculate(a, b, 100);
-  for (int i = 0; i < 100; i++) {
-    for (int j = 0; j < 100; j++)
-      cout << ans[i][j] << " ";
-    cout << endl;
+  ofstream out("output.txt");
+
+  int**ans = calculate(a, b, SIZE);
+  for (int i = 0; i < SIZE; i++) {
+    if (terminateCounter) {
+      stop_interface = true;
+      return 0;
+    }
+    for (int j = 0; j < SIZE; j++)
+      out << ans[i][j] << " ";
+    out << endl;
   }
   stop_interface = true;
   return 0;
@@ -47,23 +60,22 @@ DWORD WINAPI counterThread(LPVOID param) {
 DWORD WINAPI interfaceThread(LPVOID param) {
   cout << "Counting..." << endl;
   double lastValue = progress;
-  while (!stop_interface)if (progress - lastValue > 0.01) {
-    printf("%.2f%%\n", progress*100);
-    lastValue = progress;
+  string s;
+  while (!stop_interface) {
+    cin >> s;
+    if (s == "terminate")terminateCounter = true;
   }
   printf("%.2f%%\n", progress * 100);
   return 0;
 }
 
 int main() {
-  stop_interface = false;
+  stop_interface = terminateCounter = false;
   progress = 0;
   HANDLE counter_thread,interface_thread;
   DWORD counterId,interfaceId;
   interface_thread = CreateThread(NULL, 0, interfaceThread, NULL, 0, &interfaceId);
-  cout << "1" << endl;
   counter_thread = CreateThread(NULL, 0, counterThread, NULL, 0, &counterId);
-  cout << "2" << endl;
   if (WaitForSingleObject(counter_thread, INFINITE) == WAIT_FAILED) {
     cout << "WAIT FAILED for counter thread" << endl;
   } else {
